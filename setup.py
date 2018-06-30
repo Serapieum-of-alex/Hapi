ec1501267bd6889976e733653cc679726212d95d
Mostafa <33198578+MAfarrag@users.noreply.github.com>
2018-06-30 03:26:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Mostafa
+"""
+from setuptools import setup, find_packages
+
+with open('requirement.txt') as f:
+    Dependencies = f.readlines()
+
+setup(
+      name='Hapi',
+      version='0.1.0',
+      description='Distributed Hydrological model',
+      author='Mostafa Farrag',
+      author_email='moah.farag@gmail.come',
+      url='https://github.com/MAfarrag/HAPI',
+      License="MIT" ,
+      install_requires=Dependencies,
+      packages=['Hapi'],
+      zip_safe=False
+     )
