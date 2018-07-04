671d1463d15c1fbac94ba78a77341dfa992692d9
Mostafa <33198578+MAfarrag@users.noreply.github.com>
2018-07-04 23:02:43 +0200

    adding HRU function
    HRU function to distribute parameters based on a classified raster, and small edits in par3d function

--- Hapi/DistParameters.py
+++ Hapi/DistParameters.py
@@ -105,7 +105,7 @@ def calculateK(x,position,UB,LB):
 def par2d_lumpedK1_lake(par_g,raster,no_parameters,no_parameters_lake,kub,klb):
     """
     ===========================================================
-      par2d_lumpedK1(par_g,raster,no_parameters,no_par_lake,kub,klb)
+      par2d_lumpedK1_lake(par_g,raster,no_parameters,no_par_lake,kub,klb)
     ===========================================================
     this function takes a list of parameters and distribute them horizontally on number of cells
     given by a raster 
@@ -234,26 +234,38 @@ def par3d(par_g,raster,no_parameters,no_lumped_par=0,lumped_par_pos=[],
     Example:
     ----------
         EX1:totally distributed parameters
+            raster=gdal.Open("dem.tif")
             [fc, beta, etf, lp, c_flux, k, k1, alpha, perc, pcorr, Kmuskingum, Xmuskingum]    
             no_lumped_par=0
             lumped_par_pos=[]
             par_g=np.random.random(no_elem*(no_parameters-no_lumped_par))
+            
+            tot_dist_par=par3d(par_g,raster,no_parameters,no_lumped_par,lumped_par_pos,kub=1,klb=0.5)
+            
         EX2: One Lumped Parameter [K1]
+            raster=gdal.Open("dem.tif")
             given values of parameters are of this order
             [fc, beta, etf, lp, c_flux, k, alpha, perc, pcorr, Kmuskingum, Xmuskingum,k1] 
             K1 is lumped so its value is inserted at the end and its order should 
-            be after K 
+            be after K
+            
             no_lumped_par=1
             lumped_par_pos=[6]
             par_g=np.random.random(no_elem* (no_parameters-no_lumped_par))
             # insert the value of k1 at the end 
-            par_g=np.append(par_g,55)
+            par_g=np.append(par_g,0.005)
+            
+            dist_par=par3d(par_g,raster,no_parameters,no_lumped_par,lumped_par_pos,kub=1,klb=0.5)
+            
         EX3:Two Lumped Parameter [K1, Perc]
+            raster=gdal.Open("dem.tif")
             no_lumped_par=2
             lumped_par_pos=[6,8]
             par_g=np.random.random(no_elem* (no_parameters-no_lumped_par))
-            par_g=np.append(par_g,55)
-            par_g=np.append(par_g,66)
+            par_g=np.append(par_g,0.005)
+            par_g=np.append(par_g,0.006)
+            
+            dist_par=par3d(par_g,raster,no_parameters,no_lumped_par,lumped_par_pos,kub=1,klb=0.5)
     """
     # input data validation
     # data type
@@ -268,18 +280,18 @@ def par3d(par_g,raster,no_parameters,no_lumped_par=0,lumped_par_pos=[],
         if type(lumped_par_pos)==list:
             assert no_lumped_par==len(lumped_par_pos), "you have to entered"+str(no_lumped_par)+"no of lumped parameters but only"+str(len(lumped_par_pos))+" position "
         else: # if not int or list
-            assert 1==5 ,"you have one lumped parameters so the position has to be entered as a list"        
-#    elif no_lumped_par > 1:
-#        assert type(lumped_par_pos)==list, "you have one lumped parameters so the position can be entered as an integer number or in a list"
-    
-    # get the shape of the raster
-    shape_base_dem = raster.ReadAsArray().shape
+            assert 1==5 ,"you have one or more lumped parameters so the position has to be entered as a list"    
+
     # read the raster    
     raster_A=raster.ReadAsArray()
+    # get the shape of the raster
+    rows=raster.RasterYSize
+    cols=raster.RasterXSize
     # get the no_value of in the raster    
-    no_val = np.float32(raster.GetRasterBand(1).GetNoDataValue())
+    noval=np.float32(raster.GetRasterBand(1).GetNoDataValue())
+    
     # count the number of non-empty cells 
-    no_elem = np.size(raster_A[:,:])-np.count_nonzero((raster_A[raster_A==no_val])) 
+    no_elem = np.size(raster_A[:,:])-np.count_nonzero((raster_A[raster_A==noval])) 
     
     # input values
     if no_lumped_par > 0:
@@ -291,14 +303,14 @@ def par3d(par_g,raster,no_parameters,no_lumped_par=0,lumped_par_pos=[],
     # store the indeces of the non-empty cells
     celli=[]#np.ones((no_elem,2))
     cellj=[]
-    for i in range(shape_base_dem[0]): # rows
-        for j in range(shape_base_dem[1]): # columns
-            if raster_A[i,j]!= no_val:
+    for i in range(rows): # rows
+        for j in range(cols): # columns
+            if raster_A[i,j]!= noval:
                 celli.append(i)
                 cellj.append(j)
     
     # create an empty 3D array [[raster dimension], no_parameters]
-    par_2d=np.zeros([shape_base_dem[0], shape_base_dem[1], no_parameters])*np.nan
+    par_2d=np.zeros([rows, cols, no_parameters])*np.nan
     
     # parameters in array
     # remove a place for the lumped parameter (k1) lower zone coefficient    
@@ -333,8 +345,153 @@ def par3d(par_g,raster,no_parameters,no_lumped_par=0,lumped_par_pos=[],
     
     return par_2d
 
-def HRU():
+
+def HRU(par_g,raster,no_parameters,no_lumped_par=0,lumped_par_pos=[],
+                   kub=1,klb=0.5):
     """
+    ===========================================================
+      HRU(par_g, raster, no_parameters, no_lumped_par=0, lumped_par_pos=[], kub=1, klb=0.5)
+    ===========================================================
+    this function takes a list of parameters [saved as one column or generated
+    as 1D list from optimization algorithm] and distribute them horizontally on
+    number of cells given by a raster
+    the input raster should be classified raster (by numbers) into class to be used
+    to define the HRUs
+    
+    Inputs :
+    ----------
+        1- par_g:
+            [list] list of parameters
+        2- raster:
+            [gdal.dataset] classification raster to get the spatial information
+            of the catchment and the to define each cell belongs to which HRU
+        3- no_parameters
+            [int] no of parameters of the cell according to the rainfall runoff model
+        4-no_lumped_par:
+            [int] nomber of lumped parameters, you have to enter the value of 
+            the lumped parameter at the end of the list, default is 0 (no lumped parameters)
+        5-lumped_par_pos:
+            [List] list of order or position of the lumped parameter among all
+            the parameters of the lumped model (order starts from 0 to the length 
+            of the model parameters), default is [] (empty), the following order
+            of parameters is used for the lumped HBV model used
+            [ltt, utt, rfcf, sfcf, ttm, cfmax, cwh, cfr, fc, beta, e_corr, etf, lp,
+            c_flux, k, k1, alpha, perc, pcorr, Kmuskingum, Xmuskingum]
+        6- kub:
+            [float] upper bound of K value (traveling time in muskingum routing method)
+            default is 1 hour 
+        7- klb:
+            [float] Lower bound of K value (traveling time in muskingum routing method)
+            default is 0.5 hour (30 min)
+    
+    Output:
+    ----------
+        1- par_3d: 3D array of the parameters distributed horizontally on the cells
+        
+    Example:
+    ----------
+        EX1:HRU without lumped parameters
+            [fc, beta, etf, lp, c_flux, k, k1, alpha, perc, pcorr, Kmuskingum, Xmuskingum]
+            raster = gdal.Open("soil_types.tif")
+            no_lumped_par=0
+            lumped_par_pos=[]
+            par_g=np.random.random(no_elem*(no_parameters-no_lumped_par))
+            
+            par_hru=HRU(par_g,raster,no_parameters,no_lumped_par,lumped_par_pos,kub=1,klb=0.5)
+                
+            
+        EX2: HRU with one lumped parameters
+            given values of parameters are of this order
+            [fc, beta, etf, lp, c_flux, k, alpha, perc, pcorr, Kmuskingum, Xmuskingum,k1] 
+            K1 is lumped so its value is inserted at the end and its order should 
+            be after K 
+            
+            raster = gdal.Open("soil_types.tif")
+            no_lumped_par=1
+            lumped_par_pos=[6]
+            par_g=np.random.random(no_elem* (no_parameters-no_lumped_par))
+            # insert the value of k1 at the end 
+            par_g=np.append(par_g,0.005)
+            
+            par_hru=HRU(par_g,raster,no_parameters,no_lumped_par,lumped_par_pos,kub=1,klb=0.5)
+    """
+    # input data validation
+    # data type
+    assert type(raster)==gdal.Dataset, "raster should be read using gdal (gdal dataset please read it using gdal library) "
+    assert type(par_g)==np.ndarray or type(par_g)==list, "par_g should be of type 1d array or list"
+    assert type(no_parameters)==int, " no_parameters should be integer number"
+    assert isinstance(kub,numbers.Number) , " kub should be a number"
+    assert isinstance(klb,numbers.Number) , " klb should be a number"
+    assert type(no_lumped_par)== int, "no of lumped parameters should be integer"
+    
+    if no_lumped_par>=1:
+        if type(lumped_par_pos)==list:
+            assert no_lumped_par==len(lumped_par_pos), "you have to entered"+str(no_lumped_par)+"no of lumped parameters but only"+str(len(lumped_par_pos))+" position "
+        else: # if not int or list
+            assert 1==5 ,"you have one lumped parameters so the position has to be entered as a list"
     
     
-    """
+    # read the raster    
+    raster_A=raster.ReadAsArray()
+    # get the shape of the raster
+    rows=raster.RasterYSize
+    cols=raster.RasterXSize
+    # get the no_value of in the raster    
+    noval=np.float32(raster.GetRasterBand(1).GetNoDataValue())
+
+    # count the number of non-empty cells 
+    values=list(set([int(raster_A[i,j]) for i in range(rows) for j in range(cols) if raster_A[i,j] != noval]))
+    no_elem=len(values)
+    
+    # input values
+    if no_lumped_par > 0:
+        assert len(par_g)==(no_elem*(no_parameters-no_lumped_par))+no_lumped_par,"As there is "+str(no_lumped_par)+" lumped parameters, length of input parameters should be "+str(no_elem)+"*"+"("+str(no_parameters)+"-"+str(no_lumped_par)+")"+"+"+str(no_lumped_par)+"="+str(no_elem*(no_parameters-no_lumped_par)+no_lumped_par)+" not "+str(len(par_g))+" probably you have to add the value of the lumped parameter at the end of the list"
+    else:
+        # if there is no lumped parameters
+        assert len(par_g)==no_elem*no_parameters,"As there is no lumped parameters length of input parameters should be "+str(no_elem)+"*"+str(no_parameters)+"="+str(no_elem*no_parameters)
+    
+    # store the indeces of the non-empty cells
+    celli=[]#np.ones((no_elem,2))
+    cellj=[]
+    for i in range(rows): # rows
+        for j in range(cols): # columns
+            if raster_A[i,j]!= noval:
+                celli.append(i)
+                cellj.append(j)
+    
+    # create an empty 3D array [[raster dimension], no_parameters]
+    par_2d=np.zeros([rows, cols, no_parameters])*np.nan
+    
+    # parameters in array
+    # remove a place for the lumped parameter (k1) lower zone coefficient    
+    no_parameters=no_parameters-no_lumped_par
+    
+    # create a 2d array [no_parameters, no_cells]            
+    par_arr=np.ones((no_parameters,no_elem))
+    
+    # take the parameters from the generated parameters or the 1D list and 
+    # assign them to each cell
+    for i in range(no_elem):
+        par_arr[:,i] = par_g[i*no_parameters:(i*no_parameters)+no_parameters]
+    
+    ### lumped parameters
+    if no_lumped_par > 0:
+        for i in range(no_lumped_par):
+            # create a list with the value of the lumped parameter(k1)
+            # (stored at the end of the list of the parameters)
+            pk1=np.ones((1,no_elem))*par_g[(no_parameters*np.shape(par_arr)[1])+i]
+            # put the list of parameter k1 at the 6 row    
+            par_arr=np.vstack([par_arr[:lumped_par_pos[i],:],pk1,par_arr[lumped_par_pos[i]:,:]])
+    
+    # calculate the value of k(travelling time in muskingum based on value of 
+    # x and the position and upper, lower bound of k value 
+    for i in range(no_elem):
+        par_arr[-2,i]= calculateK(par_arr[-1,i],par_arr[-2,i],kub,klb)
+    
+    # assign the parameters from the array (no_parameters, no_cells) to 
+    # the spatially corrected location in par2d each soil type will have the same
+    # generated parameters 
+    for i in range(no_elem):
+        par_2d[raster_A==values[i]]=par_arr[:,i]
+        
+    return par_2d
