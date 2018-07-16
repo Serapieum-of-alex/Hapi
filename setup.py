fbcb4c26d73cbe01e2c6c3b09a6597e96c3242ee
Mostafa Farrag <33198578+MAfarrag@users.noreply.github.com>
2018-07-16 03:10:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@
 """
 from setuptools import setup, find_packages
 
-with open('requirement.txt') as f:
-    Dependencies = f.readlines()
+#with open('requirement.txt') as f:
+#    Dependencies = f.readlines()
 
 setup(
       name='Hapi',
@@ -15,7 +15,7 @@ setup(
       author_email='moah.farag@gmail.come',
       url='https://github.com/MAfarrag/HAPI',
       License="MIT" ,
-      install_requires=Dependencies,
+      #install_requires=Dependencies,
       packages=['Hapi'],
       zip_safe=False
      )
