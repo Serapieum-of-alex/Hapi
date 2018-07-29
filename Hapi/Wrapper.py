b4db6e39a9c2d2b5a25d827153d2af8868efcf6e
Mostafa <33198578+Orlyano@users.noreply.github.com>
2018-07-29 20:21:41 +0200

    lumped model

--- Hapi/Wrapper.py
+++ Hapi/Wrapper.py
@@ -10,6 +10,7 @@ Created on Sun Apr 29 17:17:54 2018
 #%library
 import numpy as np
 import gdal
+from types import ModuleType
 
 # functions
 import DistParameters
@@ -60,6 +61,7 @@ def Dist_model(ConceptualModel,flow_acc,flow_direct,sp_prec,sp_et,sp_temp,sp_par
         2- GIS routing scheme (routing is following river network)
     
     Inputs:
+    ----------
         1-DEM:
             [gdal.dataset] DEM raster file of the catchment (clipped to the catchment only)
         2-flow_acc:
@@ -93,7 +95,9 @@ def Dist_model(ConceptualModel,flow_acc,flow_direct,sp_prec,sp_et,sp_temp,sp_par
             [numpy array] 3d array of the long term average temperature data
         13-q_0:
             [float] initial discharge m3/s
+    
     Outputs:
+    ----------
         1- st:
             [numpy array] 3d array of the 5 state variable data for each cell
         2- q_lz:
@@ -145,3 +149,70 @@ def Dist_model(ConceptualModel,flow_acc,flow_direct,sp_prec,sp_et,sp_temp,sp_par
     q_out=q_out[:-1]
     
     return st, q_out, q_uz_routed, q_lz_trans
+
+
+
+def Lumped(ConceptualModel,data,parameters,p2,init_st,snow,Routing=0, RoutingFn=[]):
+    """
+    ==========================================================
+        Lumped(ConceptualModel,data,parameters,p2,snow,Routing=None, RoutingFn=[])
+    ==========================================================
+    
+    Inputs:
+    ----------
+        1-ConceptualModel:
+            [function] conceptual model and it should contain a function called simulate
+        2-data:
+            [numpy array] meteorological data as array with the first column as precipitation
+            second as evapotranspiration, third as temperature and forth column as
+            long term average temperature
+        3- parameters: 
+            [numpy array] conceptual model parameters as array
+        4-p2:
+            [List] list of unoptimized parameters
+            p2[0] = tfac, 1 for hourly, 0.25 for 15 min time step and 24 for daily time step
+            p2[1] = catchment area in km2
+        5-init_st:
+            [list] initial state variables values [sp, sm, uz, lz, wc].
+        6-Routing:
+            [0 or 1] to decide wether t route the generated discharge hydrograph or not
+        7-RoutingFn:
+            [function] function to route the dischrge hydrograph.
+            
+    Outputs:
+    ----------
+        1- st:
+            [numpy array] 3d array of the 5 state variable data for each cell
+        2- q_lz:
+            [numpy array] 1d array of the calculated discharge.    
+    
+    Examples:
+    ----------
+        p2=[24, 1530]
+        #[sp,sm,uz,lz,wc]
+        init_st=[0,5,5,5,0]
+        snow=0
+    """
+    ### input data validation
+    # data type
+    assert isinstance(ConceptualModel,ModuleType) , "ConceptualModel should be a module or a python file contains functions "
+    assert type(data) == np.ndarray , "meteorological data should be entered in array "
+    assert callable(RoutingFn) , "routing function should be of type callable (function that takes arguments)"
+    assert np.shape(data)[1] == 3 or np.shape(data)[1] == 4," meteorological data should be of length at least 3 (prec, ET, temp) or 4(prec, ET, temp, tm) "
+    
+    # data 
+    p=data[:,0]
+    et=data[:,1]
+    t=data[:,2]
+    tm=data[:,3]
+    
+    q_sim, st = ConceptualModel.Simulate(p, t, et, parameters, p2, 
+                                                 init_st = init_st, 
+                                                 ll_temp = tm,
+                                                 q_0=None,
+                                                 snow=0)
+    
+    if Routing != 0 :
+        q_sim=RoutingFn(np.array(q_sim[:-1]), parameters[-1])
+        
+    return st, q_sim
