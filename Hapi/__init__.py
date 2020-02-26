ce52773636c9db1a5360d497cdba7f3405a35a77
Mostafa Farrag <33198578+MAfarrag@users.noreply.github.com>
2020-02-26 23:25:17 +0100

    Update __init__.py

--- Hapi/__init__.py
+++ Hapi/__init__.py
@@ -45,6 +45,7 @@ import Hapi.HBVBergestrom92
 import Hapi.RIMInputs
 import Hapi.Event
 import Hapi.River
+# module level doc-string
 __doc__ = """
 HAPI - Hydrological library for Python
 =====================================================================
