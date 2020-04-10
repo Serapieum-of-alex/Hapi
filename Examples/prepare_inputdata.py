f41764fc951efce8410dfecc374fdd343c8ab561
Mostafa Farrag <eng_mostafa_ahmed23@yahoo.com>
2020-04-10 19:21:21 +0200

    module names
    update with new module names

--- Examples/prepare_inputdata.py
+++ Examples/prepare_inputdata.py
@@ -18,27 +18,34 @@ nodatavalue from a GIS raster (DEM, flow accumulation, flow direction raster)
 and return a folder with the output rasters with a name “New_Rasters”
 """
 dem_path="Data/GIS/4000/acc4000.tif"
+
 outputpath="Data/meteodata_prepared/"
 # prec
 prec_in_path="Data/meteodata/4000/complete_dataset/prec/"
-#Inputs.PrepareInputs(dem_path,prec_in_path,outputpath+"prec")
+# Inputs.PrepareInputs(dem_path,prec_in_path,outputpath+"prec")
 
 # evap
 evap_in_path="Data/meteodata/4000/complete_dataset/evap/"
-#Inputs.PrepareInputs(dem_path,evap_in_path,outputpath+"evap")
+# Inputs.PrepareInputs(dem_path,evap_in_path,outputpath+"evap")
 # temp
 temp_in_path="Data/meteodata/4000/complete_dataset/temp/"
-#Inputs.PrepareInputs(dem_path,temp_in_path,outputpath+"temp")
+# Inputs.PrepareInputs(dem_path,temp_in_path,outputpath+"temp")
 
 """
 in case you want to manipulate the value in all the rasters of one of the inputs 
 for example evapotranspiration values in rasters downloaded from ECMWF are -ve
 and to change it to +ve in all rasters or if you want to operate any kind of function 
 in all input rasters that are in the same folder FolderCalculator can do this task
+
+"How can evaporation have both positive and negative values? 
+Evaporation is normally negative due to the convention for fluxes.
+The meteorological convention for all vertical fluxes is that downwards is positive.
+Positive evaporation represents condensation'.
+Link: https://confluence.ecmwf.int/pages/viewpage.action?pageId=111155327
 """
 evap_out_path="Data/meteodata_prepared/evap/"
 
-# define your biggest function
+# define your function
 # this function is going to take the absolute value of the values in the raster
 # through MapAlgebra function then save the new raster to a given path with the same names
 def function(args):
@@ -46,19 +53,19 @@ def function(args):
     func=np.abs
     path = args[1]
     # first function 
-    B=GIS.MapAlgebra(A,func)
-    GIS.SaveRaster(B,path)
+    B=Raster.MapAlgebra(A,func)
+    Raster.SaveRaster(B,path)
 
 folder_path = evap_out_path
 new_folder_path="data/meteodata_prepared/new_evap/"
-#GIS.FolderCalculator(folder_path,new_folder_path,function)
+# Raster.FolderCalculator(folder_path,new_folder_path,function)
 
 """
 in order to run the model all inputs have to have the same number of rows and columns
 for this purpose MatchRasterAlignment function was made to resample, change the coordinate
 system of the second raster and give it the same alignment like a source raster (DEM raster)
 """
-dem_path="Data/GIS/4000/acc4000.tif"
+
 soil_path="Data/GIS/soil/4000/soil_raster.tif"
 DEM=gdal.Open(dem_path)
 dem_A=DEM.ReadAsArray()
@@ -70,14 +77,14 @@ aligned_soil=Raster.MatchRasterAlignment(DEM,soil)
 
 # to check alignment of DEM raster compared to aligned_soil_A raster
 aligned_soil_A=aligned_soil.ReadAsArray()
-dem_A=DEM.ReadAsArray()
+
 # nodatavalue is still different and some cells are no data value in the soil type raster but it is not in the dem raster
 # to match use Match MatchNoDataValue
 # match
-dst_Aligned_M=GIS.MatchNoDataValue(DEM,aligned_soil)
+dst_Aligned_M=Raster.MatchNoDataValue(DEM,aligned_soil)
 dst_Aligned_M_A=dst_Aligned_M.ReadAsArray()
 
 # save the new raster
-GIS.SaveRaster(dst_Aligned_M,"Data/GIS/soil/4000/soil_type.tif")
+Raster.SaveRaster(dst_Aligned_M,"Data/GIS/soil/4000/soil_type.tif")
 
-#GIS.SaveRaster(dst_Aligned_M,"00inputs/GIS/4000/soil_typeِِA.tif")
+#Raster.SaveRaster(dst_Aligned_M,"00inputs/GIS/4000/soil_typeِِA.tif")
