b39da7cf9f31b10147b2aa25da3fd1f6307500a0
Mostafa Farrag <eng_mostafa_ahmed23@yahoo.com>
2020-04-22 17:50:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 from setuptools import setup , find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
-    name='Hapi',
-	packages=['Hapi'],
+    name='HAPI-Nile',
+    #packages=['Hapi'],
     version='0.1.0',
-	description='Distributed Hydrological model',
-	author='Mostafa Farrag',
-	author_email='moah.farag@gmail.come',
-	url='https://github.com/MAfarrag/HAPI',
-    keywords=['Hydrology', 'Distributed hydrological model'],	
-	License="MIT" ,
+    description='Distributed Hydrological model',
+    author='Mostafa Farrag',
+    author_email='moah.farag@gmail.come',
+    url='https://github.com/MAfarrag/HAPI',
+    keywords=['Hydrology', 'Distributed hydrological model'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    License="MIT" ,
     zip_safe=False, 
+    packages=find_packages(),
     classifiers=[	
     'Development Status :: 4 - Beta',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Natural Language :: English',
-    'Programming Language :: Python 3.7',
-	'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python ',
+    	#'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development',
     ]
 	 )
