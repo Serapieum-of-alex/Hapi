ce9a2166c6a13a7bc38842c7ae1fe2c380bfec7e
Mostafa Farrag <eng_mostafa_ahmed23@yahoo.com>
2020-05-04 12:42:02 +0200

    new module names (small letters)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='HAPI-Nile',
-    version='0.1.0',
+    version='0.2.0',
     description='Distributed Hydrological model',
     author='Mostafa Farrag',
     author_email='moah.farag@gmail.come',
