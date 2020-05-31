103ceef4c0c9f13b18aaa357f3f6643e03767c60
Mostafa Farrag <eng_mostafa_ahmed23@yahoo.com>
2020-05-31 06:09:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,12 @@ setup(
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Natural Language :: English',
-    'Programming Language :: Python ',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Software Development',
     ]
 	 )
