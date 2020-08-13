e251fcd64df5112acd8a2426c78d46499aef2f97
juancotrino <54945848+juancotrino@users.noreply.github.com>
2020-08-12 23:28:09 -0500

    Added a V2 of MatchRasterAlignment function
    The default version was not working for me. Found this function on GitHub and combined it with MatchNoDataValue function to get rasters entirely prepared

--- Hapi/raster.py
+++ Hapi/raster.py
@@ -2528,3 +2528,56 @@ def MultipleNetCDFtoTIF(start_date, end_date, src_filepath, file_first_str, file
         src_file = src_filepath + file_first_str + day + file_second_str
         out_file = dst_filepath + dst_label + day + ".tif"
         NetCDFtoTIF(out_file,src_file,dst_EPSG,no_data_value)
+        
+def MatchRasterAlignment_V2(master, slave, dst_filename):
+    """
+    ***CODE FOUND HERE: https://github.com/jgomezdans/eoldas_ng_observations/blob/master/eoldas_ng_observations/eoldas_observation_helpers.py#L29
+    This function reprojects an image (``slave``) to
+    match the extent, resolution and projection of another
+    (``master``) using GDAL. The newly reprojected image
+    is a GDAL VRT file for efficiency. A different spatial
+    resolution can be chosen by specifyign the optional
+    ``res`` parameter. The function returns the new file's
+    name.
+    Parameters
+    -------------
+    master: str 
+        A filename (with full path if required) with the 
+        master image (that that will be taken as a reference)
+    slave: str 
+        A filename (with path if needed) with the image
+        that will be reprojected
+    res: float, optional
+        The desired output spatial resolution, if different 
+        to the one in ``master``.
+    Returns
+    ----------
+    The reprojected filename
+    TODO Have a way of controlling output filename
+    """
+    slave_ds = gdal.Open( slave )
+    if slave_ds is None:
+        raise IOError("GDAL could not open slave file %s " \
+            % slave)
+    slave_proj = slave_ds.GetProjection()
+    slave_geotrans = slave_ds.GetGeoTransform()
+    data_type = slave_ds.GetRasterBand(1).DataType
+    n_bands = slave_ds.RasterCount
+
+    master_ds = gdal.Open( master )
+    if master_ds is None:
+        raise IOError("GDAL could not open master file %s " \
+            % master)
+    master_proj = master_ds.GetProjection()
+    master_geotrans = master_ds.GetGeoTransform()
+    w = master_ds.RasterXSize
+    h = master_ds.RasterYSize
+
+    dst_ds = gdal.GetDriverByName('GTiff').Create(dst_filename, w, h, n_bands, data_type)
+    dst_ds.SetGeoTransform(master_geotrans)
+    dst_ds.SetProjection(master_proj)
+
+    gdal.ReprojectImage(slave_ds, dst_ds, slave_proj, master_proj, gdal.GRA_NearestNeighbour)
+    dst_Aligned_M = MatchNoDataValue(master_ds, dst_ds)
+    SaveRaster(dst_Aligned_M, dst_filename)
+    dst_ds = None  # Flush to disk
