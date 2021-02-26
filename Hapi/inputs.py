634f1427268f8bbc804e1740e5eb85a7118ce6bb
Mostafa Farrag <eng_mostafa_ahmed23@yahoo.com>
2021-02-26 06:41:50 +0100

    CreateLumpedInputs method
    create a method that reads the rasters of any meteorological input and take the average ti create a lumped values

--- Hapi/inputs.py
+++ Hapi/inputs.py
@@ -4,8 +4,6 @@ Created on Wed May 16 03:50:00 2018
 
 @author: Mostafa
 """
-#%links
-
 #%library
 import os
 import datetime as dt
@@ -13,7 +11,7 @@ import numpy as np
 import shutil
 import pandas as pd
 from datetime import datetime
-import geopandas as gpd
+# import geopandas as gpd
 from rasterstats import zonal_stats
 import rasterio
 from rasterio.plot import show
@@ -22,10 +20,10 @@ import Hapi
 from Hapi.raster import Raster as raster
 
 class Inputs():
-    
+
     def __init__(self):
         pass
-    
+
     @staticmethod
     def PrepareInputs(Rasteri,InputFolder,FolderName):
         """
@@ -35,7 +33,7 @@ class Inputs():
         this function prepare downloaded raster data to have the same align and
         nodatavalue from a GIS raster (DEM, flow accumulation, flow direction raster)
         and return a folder with the output rasters with a name "New_Rasters"
-    
+
         Inputs:
             1-Raster:
                 [String] path to the spatial information source raster to get the spatial information
@@ -69,9 +67,9 @@ class Inputs():
             # if not able to create the folder delete the folder with the same name and create one empty
             shutil.rmtree(os.path.join(os.environ['TEMP']+"/AllignedRasters"))
             os.makedirs(os.path.join(os.environ['TEMP'],"AllignedRasters"))
-    
+
         temp=os.environ['TEMP']+"/AllignedRasters/"
-    
+
         # match alignment
         print("First alligned files will be created in a folder 'AllignedRasters' in the Temp folder in you environment variable")
         raster.MatchDataAlignment(Rasteri,InputFolder,temp)
@@ -79,34 +77,34 @@ class Inputs():
         try:
             os.makedirs(os.path.join(os.getcwd(),FolderName))
         except WindowsError:
-            print("please function is trying to create a folder with a name"+ str(FolderName) +"New_Rasters to complete the process if there is a folder with the same name please rename it to other name")
+            assert False, "please The function is trying to create a folder with a name"+ str(FolderName) +"New_Rasters to complete the process if there is a folder with the same name please rename it to other name"
         # match nodata value
         print("second matching NoDataValue from the DEM raster too all raster will be created in the outputpath")
         raster.MatchDataNoValuecells(Rasteri,temp,FolderName+"/")
         # delete the processing folder from temp
         shutil.rmtree(temp)
-    
-    
+
+
     @staticmethod
     def ExtractParametersBoundaries(Basin):
         """
         =====================================================
             ExtractParametersBoundaries(Basin)
         =====================================================
-    
+
         Parameters
         ----------
         Basin : [Geodataframe]
             gepdataframe of catchment polygon, make sure that the geodataframe contains
             one row only, if not merge all the polygons in the shapefile first.
-    
+
         Returns
         -------
         UB : [list]
             list of the upper bound of the parameters.
         LB : [list]
             list of the lower bound of the parameters.
-    
+
         the parameters are
             ["tt", "sfcf","cfmax","cwh","cfr","fc","beta",
              "lp","k0","k1","k2","uzl","perc", "maxbas"]
@@ -115,7 +113,7 @@ class Inputs():
         ParametersPath = ParametersPath + "/Parameters"
         ParamList = ["tt", "sfcf","cfmax","cwh","cfr","fc","beta", #"rfcf","e_corr",
                  "lp","k0","k1","k2","uzl","perc", "maxbas"] #,"c_flux"
-    
+
         raster = rasterio.open(ParametersPath + "/max/" + ParamList[0] + "-Max.tif")
         Basin = Basin.to_crs(crs=raster.crs)
         # max values
@@ -125,7 +123,7 @@ class Inputs():
             array = raster.read(1)
             affine = raster.transform
             UB.append(zonal_stats(Basin, array, affine=affine, stats=['max'])[0]['max']) #stats=['min', 'max', 'mean', 'median', 'majority']
-    
+
         # min values
         LB = list()
         for i in range(len(ParamList)):
@@ -133,36 +131,36 @@ class Inputs():
             array = raster.read(1)
             affine = raster.transform
             LB.append(zonal_stats(Basin, array, affine=affine, stats=['min'])[0]['min'])
-    
+
         # plot the given basin with the parameters raster
-    
+
         # Plot DEM
         ax = show((raster, 1), with_bounds=True)
         Basin.plot(facecolor='None', edgecolor='blue', linewidth=2, ax=ax)
         # ax.set_xbound([Basin.bounds.loc[0,'minx']-10,Basin.bounds.loc[0,'maxx']+10])
         # ax.set_ybound([Basin.bounds.loc[0,'miny']-1, Basin.bounds.loc[0,'maxy']+1])
-    
+
         return UB, LB
-    
-    
+
+
     @staticmethod
     def ExtractParameters(Basin,scenario):
         """
         =====================================================
             ExtractParameters(Basin)
         =====================================================
-    
+
         Parameters
         ----------
         Basin : [Geodataframe]
             gepdataframe of catchment polygon, make sure that the geodataframe contains
             one row only, if not merge all the polygons in the shapefile first.
-    
+
         Returns
         -------
         Parameters : [list]
             list of the upper bound of the parameters.
-    
+
         the parameters are
             ["tt", "sfcf","cfmax","cwh","cfr","fc","beta",
              "lp","k0","k1","k2","uzl","perc", "maxbas"]
@@ -171,7 +169,7 @@ class Inputs():
         ParametersPath = ParametersPath + "/Parameters/" + scenario
         ParamList = ["Par_tt", "Par_sfcf","Par_cfmax","Par_cwh","Par_cfr","Par_fc","Par_beta", #"rfcf","e_corr",
                  "Par_lp","Par_k0","Par_k1","Par_k2","Par_uzl","Par_perc", "Par_maxbas"] #,"c_flux"
-    
+
         raster = rasterio.open(ParametersPath + "/" + ParamList[0] + ".tif")
         Basin = Basin.to_crs(crs=raster.crs)
         # max values
@@ -181,18 +179,31 @@ class Inputs():
             array = raster.read(1)
             affine = raster.transform
             Par.append(zonal_stats(Basin, array, affine=affine, stats=['max'])[0]['max']) #stats=['min', 'max', 'mean', 'median', 'majority']
-    
+
         # plot the given basin with the parameters raster
-    
+
         # Plot DEM
         ax = show((raster, 1), with_bounds=True)
         Basin.plot(facecolor='None', edgecolor='blue', linewidth=2, ax=ax)
         # ax.set_xbound([Basin.bounds.loc[0,'minx']-10,Basin.bounds.loc[0,'maxx']+10])
         # ax.set_ybound([Basin.bounds.loc[0,'miny']-1, Basin.bounds.loc[0,'maxy']+1])
-    
+
         return Par
-    
-    
+
+    @staticmethod
+    def CreateLumpedInputs(Path):
+        # data type
+        assert type(Path) == str, "PrecPath input should be string type"
+        # check wether the path exists or not
+        assert os.path.exists(Path), Path + " you have provided does not exist"
+        # check wether the folder has the rasters or not
+        assert len(os.listdir(Path)) > 0, Path+" folder you have provided is empty"
+        # read data
+        data = raster.ReadRastersFolder(Path)
+        data = data.mean(axis = 0)
+        data = data.mean(0)
+        return data
+
     @staticmethod
     def RenameFiles(Path, fmt = '%Y.%m.%d'):
         """
@@ -202,60 +213,60 @@ class Inputs():
         RenameFiles method takes the path to a folder where you want to put a number
         at the begining of the raster names indicating the order of the raster based on
         its date
-    
+
         Parameters
         ----------
         Path : [String]
             path where the rasters are stored.
         fmt : [String], optional
             the format of the date. The default is '%Y.%m.%d'.
-    
+
         Returns
         -------
         files in the Path are going to have a new name including the order at
         the begining of the name.
-    
+
         """
-    
+
         files = os.listdir(Path)
         if "desktop.ini" in files: files.remove("desktop.ini")
-    
+
         # get the date
         dates_str = [files[i].split("_")[-1][:-4] for i in range(len(files))]
         dates = [datetime.strptime(dates_str[i], fmt) for i in range(len(files))]
-    
+
         df = pd.DataFrame()
         df['files'] = files
         df['DateStr'] = dates_str
         df['dates'] = dates
         df.sort_values('dates', inplace=True)
-    
+
         df['order'] = [i for i in range(len(files))]
-    
+
         df['new_names'] = [str(df.loc[i,'order']) + "_"+ df.loc[i,'files'] for i in range(len(files))]
         # rename the files
         for i in range(len(files)):
             os.rename(Path + "/" + df.loc[i,'files'], Path + "/" + df.loc[i,'new_names'])
-    
-    
+
+
     @staticmethod
     def changetext2time(string):
         """
-        # =============================================================================
-        #     changetext2time(string)
-        # =============================================================================
+        ============================================================
+            changetext2time(string)
+        ============================================================
         this functions changes the date from a string to a date time format
         """
         time=dt.datetime(int(string[:4]),int(string[5:7]),int(string[8:10]),
                       int(string[11:13]),int(string[14:16]),int(string[17:]))
         return time
-    
+
     @staticmethod
     def rescale(OldValue,OldMin,OldMax,NewMin,NewMax):
         """
-        # =============================================================================
-        #  rescale(OldValue,OldMin,OldMax,NewMin,NewMax)
-        # =============================================================================
+        ===================================================================
+            rescale(OldValue,OldMin,OldMax,NewMin,NewMax)
+        ===================================================================
         this function rescale a value between two boundaries to a new value bewteen two
         other boundaries
         inputs:
@@ -272,20 +283,20 @@ class Inputs():
         output:
             1-NewValue:
                 [float] transformed new value
-    
+
         """
         OldRange = (OldMax - OldMin)
         NewRange = (NewMax - NewMin)
         NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
-    
+
         return NewValue
-    
+
     @staticmethod
     def mycolor(x,min_old,max_old,min_new, max_new):
         """
-        # =============================================================================
-        #  mycolor(x,min_old,max_old,min_new, max_new)
-        # =============================================================================
+        ===================================================================
+             mycolor(x,min_old,max_old,min_new, max_new)
+        ===================================================================
         this function transform the value between two normal values to a logarithmic scale
         between logarithmic value of both boundaries
         inputs:
@@ -303,25 +314,25 @@ class Inputs():
             1-Y:
                 [int] integer number between new max_new and min_new boundaries
         """
-    
+
         # get the boundaries of the logarithmic scale
         if min_old== 0.0:
             min_old_log=-7
         else:
             min_old_log=np.log(min_old)
-    
+
         max_old_log=np.log(max_old)
-    
+
         if x==0:
             x_log=-7
         else:
             x_log=np.log(x)
-    
+
         y=int(np.round(Inputs.rescale(x_log,min_old_log,max_old_log,min_new,max_new)))
-    
+
         return y
 
-    @staticmethod    
+    @staticmethod
     def ReadExcelData(path,years,months):
         """
         ===========================================================
@@ -332,7 +343,7 @@ class Inputs():
         year month 1 2 3 4 5 6 7 8 9 .....................31
         2012  1    5 6 2 6 8 6 9 7 4 3 ...................31
         2012  2    9 8 7 6 3 2 1 5 5 9 ...................31
-    
+
         inputs:
         ----------
             1- path:
@@ -350,7 +361,7 @@ class Inputs():
             months=[1,2,3,4,5,6,7,8,9,10,11,12]
             Q=ReadExcelData(path+"Discharge/Qout.xlsx",years,months)
         """
-    
+
         Qout=pd.read_excel(path)
         Q=[]
     #    years=[2009,2010,2011]#,2012,2013]
@@ -361,7 +372,8 @@ class Inputs():
                 row=row.drop(['year','month'], axis=1)
                 row=row.values.tolist()[0]
                 Q=Q+row
-    
+
         Q=[Q[i] for i in range(len(Q)) if not np.isnan(Q[i])]
-    
+
         return Q
+
