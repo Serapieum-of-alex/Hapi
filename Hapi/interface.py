34090d81b0fd9992742e4ac36f8c505b2aa9fd4f
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-03-18 01:41:14 +0100

    boundary conditions methods
    - ReadBoundaryConditionsTable
    -ReadBoundaryConditions

--- Hapi/interface.py
+++ Hapi/interface.py
@@ -108,9 +108,87 @@ class Interface(River):
         end = self.ReferenceIndex.loc[ToDay,'date']
     
         self.Laterals.index = pd.date_range(start, end, freq = 'D')
+    
+    def ReadBoundaryConditionsTable(self, Path):
+        """
+        ===============================================================
+               ReadLateralsTable(Path)
+        ===============================================================
+        ReadLateralsTable method reads the laterals file 
+            laterals file : file contains the xsid of the cross-sections that 
+            has laterals
+        if the user has already read te cross section file, the methos is going 
+        to add column to the crosssection dataframe attribute and is going to add
+        a value of 1 to the xs that has laterals
+        
+        Parameters
+        ----------
+        Path : [String], optional
+            Path to read the results from.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.BC = pd.read_csv(Path, skiprows=[0], header=None)
+        self.BC.columns = ["xsid"]
+            
+    
+    def ReadBoundaryConditions(self, FromDay = '', ToDay = '', Path = '',
+                          date_format="'%Y-%m-%d'"):
+        """
+        =======================================================================
+            ReadUSHydrograph(FromDay = '', ToDay = '', Path = '',
+                                date_format="'%Y-%m-%d'")
+        =======================================================================
+
+        Parameters
+        ----------
+        1-FromDay : [integer], optional
+                the day you want to read the result from, the first day is 1 not zero.The default is ''.
+        2-ToDay : [integer], optional
+                the day you want to read the result to.
+        3-Path : [String], optional
+            Path to read the results from. The default is ''.
+        4-date_format : "TYPE, optional
+            DESCRIPTION. The default is "'%Y-%m-%d'".
+
+        Returns
+        -------
+        1-USHydrographs : [dataframe attribute].
+            dataframe contains the hydrograph of each of the upstream segments
+            with segment id as a column name and a column 'total' contains the 
+            sum of all the hydrographs.
+        """        
+        assert hasattr(self, 'LateralsTable'), "Please read the lateras table first using the 'ReadLateralsTable' method"
+        
+        # if Path == '':
+            # Path = self.CustomizedRunsPath
+        
+        self.Laterals = pd.DataFrame()
+        
+        for i in range(len(self.LateralsTable)):
+            NodeID = self.LateralsTable.loc[i,'xsid']
+            fname = "LF_xsid" + str(NodeID)
+            self.Laterals[NodeID]  = self.ReadRRMResults(self.Version, self.ReferenceIndex, 
+                                                            Path, fname, FromDay, ToDay,
+                                                            date_format)[fname].tolist()
+            
+            
+        self.Laterals['total'] = self.Laterals.sum(axis=1)
+        if FromDay == '':
+            FromDay = 1
+        if ToDay == '':
+            ToDay = len(self.Laterals[NodeID])
+    
+        start = self.ReferenceIndex.loc[FromDay,'date']
+        end = self.ReferenceIndex.loc[ToDay,'date']
+    
+        self.Laterals.index = pd.date_range(start, end, freq = 'D')
         
     
-        
+            
         
     
         
