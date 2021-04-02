ae96e91d5e7a1b30b9d6556a0e9c15be08718adb
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-04-02 09:37:12 +0200

    average calculation
    add average calculation

--- Examples/Create Inputs/Max_MinParametervalues.py
+++ Examples/Create Inputs/Max_MinParametervalues.py
@@ -7,23 +7,28 @@ of the HBV-SIMREG
 """
 from IPython import get_ipython
 get_ipython().magic("reset -f")
+# Comp = "F:/"
+Comp = "F:/Users/mofarrag/Documents/"
 # import os
 import gdal
 # import osr
 # import numpy as np
 # import gdalconst
-import Hapi.raster as R
+from Hapi.raster import Raster as R
 
 par = "Par_UZL"
-path = "F:/01Algorithms/HBV-SIMREG/parameters/" + par
+path = Comp + "01Algorithms/HBV-SIMREG/parameters/" + par
 parameters = R.ReadRastersFolder(path, WithOrder=False)
-MaxValue = parameters.max(axis=2)
-MinValue = parameters.min(axis=2)
+# MaxValue = parameters.max(axis=2)
+# MinValue = parameters.min(axis=2)
+MeanValue = parameters.mean(axis=2)
 
 Path1 = path + "/" + par + "-1.tif"
-Saveto1 = path + "/" + par + "-Max.tif"
-Saveto2 = path + "/" + par + "-Min.tif"
+# Saveto1 = path + "/" + par + "-Max.tif"
+# Saveto2 = path + "/" + par + "-Min.tif"
+Saveto3 = path + "/" + par + "-mean.tif"
 src = gdal.Open(Path1)
 
-R.RasterLike(src,MaxValue,Saveto1,pixel_type=1)
-R.RasterLike(src,MinValue,Saveto2,pixel_type=1)
+# R.RasterLike(src,MaxValue,Saveto1,pixel_type=1)
+# R.RasterLike(src,MinValue,Saveto2,pixel_type=1)
+R.RasterLike(src,MeanValue,Saveto3,pixel_type=1)
