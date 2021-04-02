30e9f1507298c9e0023ce30d690cc3f7c27a7211
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-04-02 05:37:46 +0200

    convert netcdf with multiple layers inside

--- Examples/GIS/NetCdf_toRaster.py
+++ Examples/GIS/NetCdf_toRaster.py
@@ -4,29 +4,46 @@ Created on Sun Aug  2 22:30:48 2020
 
 @author: mofarrag
 
+make sure to change the directory to the Examples folder in the repo
 """
-from IPython import get_ipython
-get_ipython().magic("reset -f")
+
+# from IPython import get_ipython
+# get_ipython().magic("reset -f")
 from Hapi.raster import Raster
 import rasterio
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import os
+ParentPath =  "F:/Users/mofarrag/Documents/01Algorithms/HAPI/Examples/"
+#%% Netcdf file that contains only one layer 
+# FileName = ParentPath + "/data/GIS/MSWEP_1979010100.nc"
+# SaveTo = ParentPath + "/data/GIS/"
+# VarName=None
 
-#%% Paths
-FileName = "F:/01Algorithms/Hydrology/HAPI/Examples/data/GIS/MSWEP_1979010100.nc"
-SaveTo = "F:/01Algorithms/Hydrology/HAPI/Examples/data/GIS/"
-VarName=None
-#%%
-Raster.NCtoTiff(FileName, SaveTo)
+# Raster.NCtoTiff(FileName, SaveTo, Separator="_")
 
-#%% plot
+# plot
 
-src = rasterio.open(SaveTo + "MSWEP_1979010100.nc")
-fig = plt.figure(figsize=(12,8))
-im = plt.imshow(src.read(1)/100.0, cmap="gist_rainbow")
-plt.title("Monthly mean sea level pressure")
-divider = make_axes_locatable(plt.gca())
-cax = divider.append_axes("right", "5%", pad="3%")
-plt.colorbar(im, cax=cax)
-plt.tight_layout()
-plt.show()
+# src = rasterio.open(SaveTo + "MSWEP_1979010100.nc")
+# fig = plt.figure(figsize=(12,8))
+# im = plt.imshow(src.read(1)/100.0, cmap="gist_rainbow")
+# plt.title("Monthly mean sea level pressure")
+# divider = make_axes_locatable(plt.gca())
+# cax = divider.append_axes("right", "5%", pad="3%")
+# plt.colorbar(im, cax=cax)
+# plt.tight_layout()
+# plt.show()
+#%% Netcdf file that contains multiple layer 
+# FileName =  ParentPath + "/data/GIS/precip.1979.nc"
+# SaveTo = ParentPath + "/data/GIS/Save_prec_netcdf_multiple/"
+
+# Raster.NCtoTiff(FileName, SaveTo, Separator=".")
+#%% list of files
+Path =  ParentPath + "/data/GIS/netcdf files/"
+SaveTo = ParentPath + "/data/GIS/Save_prec_netcdf_multiple/"
+
+files = os.listdir(Path)
+Paths = [Path + i for i in files]
+for i in range(len(files)):
+    FileName = Path + "/" + files[i]
+    Raster.NCtoTiff(FileName, SaveTo, Separator=".")
