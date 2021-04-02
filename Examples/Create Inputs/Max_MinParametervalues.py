aa3f4f81893cd3cff2514354175652703f65200d
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-04-02 11:33:19 +0200

    update parameters

--- Examples/Create Inputs/Max_MinParametervalues.py
+++ Examples/Create Inputs/Max_MinParametervalues.py
@@ -16,19 +16,34 @@ import gdal
 # import gdalconst
 from Hapi.raster import Raster as R
 
-par = "Par_UZL"
-path = Comp + "01Algorithms/HBV-SIMREG/parameters/" + par
-parameters = R.ReadRastersFolder(path, WithOrder=False)
-# MaxValue = parameters.max(axis=2)
-# MinValue = parameters.min(axis=2)
-MeanValue = parameters.mean(axis=2)
-
-Path1 = path + "/" + par + "-1.tif"
-# Saveto1 = path + "/" + par + "-Max.tif"
-# Saveto2 = path + "/" + par + "-Min.tif"
-Saveto3 = path + "/" + par + "-mean.tif"
-src = gdal.Open(Path1)
-
-# R.RasterLike(src,MaxValue,Saveto1,pixel_type=1)
-# R.RasterLike(src,MinValue,Saveto2,pixel_type=1)
-R.RasterLike(src,MeanValue,Saveto3,pixel_type=1)
+ParamList = ["01_tt", "02_rfcf", "03_sfcf","04_cfmax","05_cwh","06_cfr","07_fc","08_beta", "09_etf",
+                 "10_lp","11_k0","12_k1","13_k2","14_uzl","15_perc", "16_maxbas", "17_K_muskingum", 
+                 "18_x_muskingum"]
+SaveTo = Comp + "01Algorithms/HAPI/Hapi/Parameters/" 
+# par = "UZL"
+for i in range(len(ParamList)):
+    Path = list()
+    for j in range(0,10):
+        if j < 9 : 
+            folder = "0" + str(j+1)
+        else:
+            folder = str(j+1)
+            
+        Path.append(Comp + "01Algorithms/HAPI/Hapi/Parameters/" + folder + "/" + ParamList[i]+ ".tif") 
+        
+    parameters = R.ReadRastersFolder(Path, WithOrder=False)
+    MaxValue = parameters.max(axis=2)
+    MinValue = parameters.min(axis=2)
+    MeanValue = parameters.mean(axis=2)
+    
+    # Path1 = path + "/" + ParamList[i] + "-1.tif"
+    src = gdal.Open(Path[0])
+    
+    Saveto1 = SaveTo + "/max/" + ParamList[i] + ".tif"
+    Saveto2 = SaveTo  + "/min/" + ParamList[i] + ".tif"
+    Saveto3 = SaveTo  + "/avg/" + ParamList[i] + ".tif"
+    
+    
+    R.RasterLike(src,MaxValue,Saveto1,pixel_type=1)
+    R.RasterLike(src,MinValue,Saveto2,pixel_type=1)
+    R.RasterLike(src,MeanValue,Saveto3,pixel_type=1)
