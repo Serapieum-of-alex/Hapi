088f85609de71f33f82b0b31b53e6b41c245cc1b
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-04-11 02:31:58 +0200

    update new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='HAPI-Nile',
-    version='1.0.2',
+    version='1.0.3',
     description='Distributed Hydrological model',
     author='Mostafa Farrag',
     author_email='moah.farag@gmail.come',
