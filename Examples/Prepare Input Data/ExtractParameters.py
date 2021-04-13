abd807e1d70d1e5aaa1bf2c777c537d16033cb49
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-04-14 01:07:26 +0200

    update examples

--- Examples/Prepare Input Data/ExtractParameters.py
+++ Examples/Prepare Input Data/ExtractParameters.py
@@ -5,12 +5,12 @@ Created on Sun Mar 14 18:45:56 2021
 @author: mofarrag
 """
 import os
-Comp = "F:/Users/mofarrag/"
+Comp = "F:/01Algorithms/Hydrology/HAPI/Examples/"
 os.chdir(Comp + "/Coello/HAPI/Data")
 
 from Hapi.inputs import Inputs
 
-dem_path = "00inputs/GIS/4000/acc4000.tif"
+dem_path = "Data/GIS/Hapi_GIS_Data/acc4000.tif"
 outputpath = Comp + "coello/Hapi/Model/results/parameters/00default parameters/mean/"
 
 Inputs.ExtractParameters(dem_path,'avg', AsRaster=True, SaveTo=outputpath)
