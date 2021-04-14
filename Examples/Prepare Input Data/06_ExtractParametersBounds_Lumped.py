03b134a954436df594fc56ac4f2743bca8289416
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-04-14 02:20:24 +0200

    update examples

---
+++ Examples/Prepare Input Data/06_ExtractParametersBounds_Lumped.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 22:15:47 2020
+
+@author: mofarrag
+"""
+"""
+Make sure the working directory is set to the examples folder in the Hapi repo"
+currunt_work_directory = Hapi/Example
+"""
+import os
+os.chdir("F:/01Algorithms/Hydrology/HAPI/Examples/")
+import geopandas as gpd
+# import numpy as np
+# import pandas as pd
+from Hapi.inputs import Inputs as IN
+
+BasinF = "data/GIS/Hapi_GIS_Data/BasinExtractParameters.shp"
+SaveTo = "data/parameters"
+#%%
+Basin = gpd.read_file(BasinF)
+# parameters name with the same order inside the Input module
+# ind = ["tt","sfcf","cfmax","cwh","cfr","fc","beta","lp","k0","k1","k2","uzl","perc","maxbas"]
+# Par = pd.DataFrame(index = ind)
+# extract parameters boundaries
+Par = IN.ExtractParametersBoundaries(Basin)
+
+# extract parameters in a specific scenarion from the 10 scenarios
+Par['1'] = IN.ExtractParameters(Basin,"10")
+"""
+zoom to the place where the catchment exist to check if the basin polygon overlay
+the right location, if not there is a problem in the coordinate reference system
+transformation
+"""
+#%% save the parameters
+Par['UB'].to_csv(SaveTo + "/UB-Extracted.txt", header=None,float_format="%4.3f")
+Par['LB'].to_csv(SaveTo + "/LB-Extracted.txt", header=None,float_format="%4.3f")
+Par['1'].to_csv(SaveTo + "/scenario1.txt", header=None,float_format="%4.3f")
--- Examples/Prepare Input Data/07_prepareRasters.py
+++ Examples/Prepare Input Data/07_prepareRasters.py
@@ -3,17 +3,16 @@
 Created on Sat Mar 27 19:09:20 2021
 
 @author: mofarrag
+
+Make sure the working directory is set to the examples folder in the Hapi repo"
+currunt_work_directory = Hapi/Example
+
 """
-import os
-Comp = "F:/Users/mofarrag/"
-os.chdir(Comp + "/Coello/HAPI/Data")
 from Hapi.raster import Raster
 import gdal
-import numpy as np
 
-dem_path="00inputs/GIS/4000/acc4000.tif"
-# SaveTo = "F:/Users/mofarrag/coello/Hapi/Data/00inputs/Basic_inputs/default parameters/01/"
-SaveTo = "F:/Users/mofarrag/coello/Hapi/Model/results/parameters/4000/lumped/2021-03-30/rasters/"
+dem_path = "Data/GIS/Hapi_GIS_Data/acc4000.tif"
+SaveTo = "data/parameters/"
 #%%
 'craeate a raster typicall to the DEM and fill it with 1'
 K = 1
@@ -24,6 +23,4 @@ Raster.RasterFill(src, K, SaveTo+'11_K_muskingum.tif')
 X = 0.2
 
 Raster.RasterFill(src, X, SaveTo+'12_X_muskingum.tif')
-
-#%%
 
