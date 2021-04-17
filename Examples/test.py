697db161ae8f36ee717bd51f31997be72c416105
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-04-17 22:34:08 +0200

    update travis tests

--- Examples/test.py
+++ Examples/test.py
@@ -4,12 +4,14 @@ Created on Sun Jan 19 16:34:44 2020
 
 @author: mofarrag
 """
-try:
-    import Hapi
-except ImportError:
-	try:
-		import HAPI
-	except ImportError:
-	    import sys
-	    sys.path.append(".")
-	    import Hapi
+import Hapi
+
+#try:
+#    import Hapi
+#except ImportError:
+#	try:
+#		import HAPI
+#	except ImportError:
+#	    import sys
+#	    sys.path.append(".")
+#	    import Hapi
