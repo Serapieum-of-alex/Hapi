ecab354cdc67f86c68c96bfb48ac007a1d1ce4db
Mostafa Farrag <mofarrag@gfz-potsdam.de>
2021-05-11 05:41:53 +0200

    update

--- Hapi/rrm/__init__.py
+++ Hapi/rrm/__init__.py
@@ -8,11 +8,11 @@ Created on Sat Apr 24 20:57:45 2021
 from .calibration import *
 from .distparameters import *
 from .distrrm import *
-from .hbv import *
-from .hbv_bergestrom92 import *
-from .hbv_lake import *
-from .hbvlumped import *
-from .hbvold import *
+# from .hbv import *
+# from .hbv_bergestrom92 import *
+# from .hbv_lake import *
+# from .hbvlumped import *
+# from .hbvold import *
 from .inputs import *
 from .routing import *
 from .run import *
