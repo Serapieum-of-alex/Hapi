b53736d6fd2f2b8c95d238a99774da70fa266af6
Mostafa Farrag <moah.farag@gmail.com>
2021-11-23 21:20:16 +0100

    move statistical examples to HapiSM

--- Hapi/statistics.py
+++ Hapi/statistics.py
@@ -1,1 +1,6 @@
 from HapiSM import *
+from HapiSM import distributions as distributions
+from HapiSM import parameterestimation as parameterestimation
+from HapiSM import performancecriteria as performancecriteria
+from HapiSM import sensitivityanalysis as sensitivityanalysis
+from HapiSM import statisticaltools as statisticaltools
