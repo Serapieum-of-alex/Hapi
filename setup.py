203934c330552fdc4e8bee1e512200990e3f7fa2
Mostafa Farrag <moah.farag@gmail.com>
2022-12-26 16:02:14 +0100

    remove python from requirement.txt

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [line.strip() for line in open("requirements.txt").readlines()]
-requirements = requirements[1:]
+# requirements = requirements[1:]
 
 requirements_dev = [line.strip() for line in open("requirements-dev.txt").readlines()]
 
