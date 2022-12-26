b69eb5a41c194eab18d86a6dcca1236c4736bee5
Mostafa Farrag <moah.farag@gmail.com>
2022-12-26 03:28:46 +0100

    update hydraulic model examples

--- examples/Hydrological model/Prepare Input Data/05_Extract_Distributed_Parameters.py
+++ examples/Hydrological model/Prepare Input Data/05_Extract_Distributed_Parameters.py
@@ -1,11 +1,15 @@
 """
 Make sure the working directory is set to the examples folder in the Hapi repo"
-currunt_work_directory = Hapi/Example
+currunt_work_directory = Hapi
 """
 
 from Hapi.rrm.inputs import Inputs
 
-dem_path = "Data/GIS/Hapi_GIS_Data/acc4000.tif"
-outputpath = "data/parameters/03/"
+dem_path = "examples/Hydrological model/data/gis_data/acc4000.tif"
+outputpath = "examples/Hydrological model/data/gis_data/parameters/03/"
 
+<<<<<<< Updated upstream
 Inputs.extractParameters(dem_path, "03", AsRaster=True, SaveTo=outputpath)
+=======
+Inputs.extractParameters(dem_path, "03", as_raster=True, save_to=outputpath)
+>>>>>>> Stashed changes
