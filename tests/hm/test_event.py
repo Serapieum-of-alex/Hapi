c55ef2b8e7f7f056c7f567ba78bea5494805f032
Mostafa Farrag <moah.farag@gmail.com>
2023-04-05 12:23:03 +0200

    test_get_event_start

--- tests/hm/test_event.py
+++ tests/hm/test_event.py
@@ -33,3 +33,14 @@ def test_read_event_index(
     event = Event.read_event_index("test", event_index_file, start="1955-01-01")
     assert isinstance(event.event_index, DataFrame)
     assert all([i in event.event_index.columns for i in event_index_volume_attrs2])
+
+
+def test_get_event_start(
+    event_index_file: str, volume_error_file: str, event_index_volume_attrs2: List[str]
+):
+    event = Event.read_event_index("test", event_index_file, start="1955-01-01")
+    max_overtopping_ind = event.event_index["cells"].idxmax()
+    start_ind, start_day = event.get_event_start(max_overtopping_ind)
+    end_ind, end_day = event.get_event_end(max_overtopping_ind)
+    assert start_day == 8195
+    assert end_day == 8200
