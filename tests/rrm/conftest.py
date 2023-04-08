b8e6a61823c02ea1e0665a0c9edd9d1b8c8a07ed
Mostafa Farrag <moah.farag@gmail.com>
2023-04-08 04:45:54 +0200

    test rrm calibration and catchment

--- tests/rrm/conftest.py
+++ tests/rrm/conftest.py
@@ -1,2 +1,11 @@
 from tests.rrm.calibration.conftest import *
 from tests.rrm.catchment.conftest import *
+
+
+@pytest.fixture(scope="module")
+def coello_start_date() -> str:
+    return "2009-01-01"
+
+@pytest.fixture(scope="module")
+def coello_end_date() -> str:
+    return "2009-01-11"
