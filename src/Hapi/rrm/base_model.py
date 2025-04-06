118ebdeea102c10fec4cb525e5b55b6be40db214
Mostafa Farrag <moah.farag@gmail.com>
2025-04-06 19:48:16 +0200

    add the `base_model.py` template conceptual model

--- src/Hapi/rrm/base_model.py
+++ src/Hapi/rrm/base_model.py
@@ -1,35 +1,55 @@
 from typing import Tuple
+
 import numpy as np
 
+
 class BaseConceptualModel:
+    """Base class for conceptual models."""
+
     def precipitation(
-            self, temp: float, ltt: float, utt: float, prec: float, rfcf: float, sfcf: float, pcorr: float = 1.0
+        self,
+        temp: float,
+        ltt: float,
+        utt: float,
+        prec: float,
+        rfcf: float,
+        sfcf: float,
+        pcorr: float = 1.0,
     ) -> Tuple[float, float]:
         """Split precipitation into rainfall and snowfall."""
         ...
 
     def snow(
-            self, temp, ttm, cfmax, cfr, cwh, rf, sf, wc_old, sp_old
+        self, temp, ttm, cfmax, cfr, cwh, rf, sf, wc_old, sp_old
     ) -> Tuple[float, float, float]:
         """Snow accumulation/melt routine."""
         ...
 
     def soil(
-            self, fc, beta, etf, temp, tm, e_corr, lp, tfac, c_flux,
-            infiltration, ep, sm_old, uz_old
+        self,
+        fc,
+        beta,
+        etf,
+        temp,
+        tm,
+        e_corr,
+        lp,
+        tfac,
+        c_flux,
+        infiltration,
+        ep,
+        sm_old,
+        uz_old,
     ) -> Tuple[float, float]:
         """Soil moisture and runoff routine."""
         ...
 
     def response(
-            self, tfac, perc, alpha, k, k1, area, lz_old, uz_int_1
+        self, tfac, perc, alpha, k, k1, area, lz_old, uz_int_1
     ) -> Tuple[float, float, float]:
         """Convert runoff to stream discharge."""
         ...
 
-    def routing(
-            self, q: np.ndarray, maxbas: int = 1
-    ) -> np.ndarray:
+    def routing(self, q: np.ndarray, maxbas: int = 1) -> np.ndarray:
         """Apply triangular routing function."""
         ...
-
