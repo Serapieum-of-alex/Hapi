68b6b099402a7d79a24b318540728a676527a656
Mostafa Farrag <moah.farag@gmail.com>
2025-04-07 23:23:41 +0200

    reformat the lake hbv bergestrom

--- src/Hapi/rrm/hbv_bergestrom92.py
+++ src/Hapi/rrm/hbv_bergestrom92.py
@@ -40,7 +40,7 @@ class HBVBergestrom92(BaseConceptualModel):
 
     @staticmethod
     def precipitation(prec, temp, tt, rfcf, sfcf):
-        """Precipitaiton routine of the HBV96 model. If temperature is lower than TT [degree C], all the precipitation is considered as snow. If the temperature is higher than tt, all the precipitation is considered as rainfall.
+        """Precipitaiton routine of the HBV96 model. If the temperature is lower than TT [degree C], all the precipitation is considered as snow. If the temperature is higher than tt, all the precipitation is considered as rainfall.
 
         Parameters
         ----------
@@ -62,13 +62,15 @@ class HBVBergestrom92(BaseConceptualModel):
         sf : float
             Snowfall [mm]
         """
-
-        if temp <= tt:  # if temp <= lower temp threshold
-            rf = 0.0  # no rainfall all the precipitation will convert into snowfall
+        # if temp <= lower temp threshold
+        if temp <= tt:
+            # no rainfall all the precipitation will convert into snowfall
+            rf = 0.0
             sf = prec * sfcf
-
-        else:  # temp >= tt: # if temp > upper threshold
-            rf = prec * rfcf  # no snowfall all the precipitation becomes rainfall
+        else:
+            # temp >= tt: # if temp > upper threshold
+            # no snowfall all the precipitation becomes rainfall
+            rf = prec * rfcf
             sf = 0.0
 
         return rf, sf
@@ -77,8 +79,8 @@ class HBVBergestrom92(BaseConceptualModel):
     def snow(temp, rf, sf, wc_old, sp_old, tt, cfmax, cfr, cwh):
         """Snow routine. The snow pack consists of two states: Water Content (wc) and Snow Pack (sp). The first corresponds to the liquid part of the water in the snow, while the latter corresponds to the solid part. If the temperature is higher than the melting point, the snow pack will melt and the solid snow will become liquid. In the opposite case, the liquid part of the snow will refreeze, and turn into solid. The water that cannot be stored by the solid part of the snow pack will drain into the soil as part of infiltration.
 
-            All precipitation simulated to be snow, i.e. falling when the temperature
-            is bellow TT, is multiplied by a snowfall correction factor, SFCF [-].
+            All precipitation simulated to be snow, i.e., falling when the temperature
+            is below TT, is multiplied by a snowfall correction factor, SFCF [-].
 
             Snowmelt is calculated with the degree-day method (cfmax). Meltwater and
             rainfall is retained within the snowpack until it exceeds a certain
@@ -116,43 +118,39 @@ class HBVBergestrom92(BaseConceptualModel):
         sp_new : float
             Snowpack in posterior state [mm]
         """
-
-        if temp > tt:  # if temp > melting threshold
+        # if temp > melting threshold
+        if temp > tt:
             # then either some snow will melt or the entire snow will melt
-            if (
-                cfmax * (temp - tt) < sp_old + sf
-            ):  # if amount of melted snow < the entire existing snow (previous amount+new)
+            if cfmax * (temp - tt) < sp_old + sf:
+                # if amount of melted snow < the entire existing snow (previous amount+new)
                 melt = cfmax * (temp - tt)
-            else:  # if amount of melted snow > the entire existing snow (previous amount+new)
-                melt = (
-                    sp_old + sf
-                )  # then the entire existing snow will melt (old snow pack + the current snowfall)
+            else:
+                # if the amount of melted snow > the entire existing snow (previous amount+new)
+                # then the entire existing snow will melt (old snow pack + the current snowfall)
+                melt = sp_old + sf
 
             sp_new = sp_old + sf - melt
             wc_int = wc_old + melt + rf
-
-        else:  # if temp < melting threshold
+        else:
+            # if temp < melting threshold,
             # then either some water will freeze or all the water willfreeze
-            if (
-                cfr * cfmax * (tt - temp) < wc_old + rf
-            ):  # then either some water will freeze or all the water willfreeze
-                refr = (
-                    cfr * cfmax * (tt - temp)
-                )  # cfmax*(ttm-temp) is the rate of melting of snow while cfr*cfmax*(ttm-temp) is the rate of freeze of melted water  (rate of freezing > rate of melting)
-            else:  # if the amount of frozen water > entire water available
+            if cfr * cfmax * (tt - temp) < wc_old + rf:
+                refr = cfr * cfmax * (tt - temp)
+                # cfmax*(ttm-temp) is the melting rate of snow while cfr*cfmax*(ttm-temp)
+                # is the freezing rate of melted water (rate of freezing > rate of melting)
+            else:
+                # if the amount of frozen water > entire water available
                 refr = wc_old + rf
 
             sp_new = sp_old + sf + refr
             wc_int = wc_old - refr + rf
 
-        #
-        if (
-            wc_int > cwh * sp_new
-        ):  # if water content > holding water capacity of the snow
-            inf = wc_int - cwh * sp_new  # water content  will infiltrate
-            wc_new = (
-                cwh * sp_new
-            )  # and the capacity of snow of holding water will retained
+        if wc_int > cwh * sp_new:
+            # if water content > holding water capacity of the snow
+            inf = wc_int - cwh * sp_new
+            # water content will infiltrate
+            wc_new = cwh * sp_new
+            # and the capacity of snow of holding water will retained
         else:  # if water content < holding water capacity of the snow
             inf = 0.0  # no infiltration
         wc_new = wc_int
@@ -161,14 +159,12 @@ class HBVBergestrom92(BaseConceptualModel):
 
     @staticmethod
     def soil(temp, inf, ep, sm_old, uz_old, tm, fc, beta, e_corr, lp):
-        """Soil soil routine of the HBV-96 model. The model checks for the amount of water that can infiltrate the soil, coming from the liquid precipitation and the snow pack melting. A part of the water will be stored as soil moisture, while other will become runoff, and routed to the upper zone tank.
+        """Soil routine of the HBV-96 model. The model checks for the amount of water that can infiltrate the soil, coming from the liquid precipitation and the snow pack melting. A part of the water will be stored as soil moisture, while other will become runoff, and routed to the upper zone tank.
 
-            -Actual evaporation from the soil box equals the potential evaporation if
-            SM/FC is above LP [%] while a linear reduction is used when SM/FC is below
-            LP.
-            -Groundwater recharge (r) is added to the upper groundwater box (UZ [mm])
-            -PERC [mm d-1]defines the maximum percolation rate from the upper to the
-            lower groundwater box (LZ [mm])
+            - Actual evaporation from the soil box equals the potential evaporation if SM/FC is above LP [%] while a
+            linear reduction is used when SM/FC is below LP.
+            - Groundwater recharge (r) is added to the upper groundwater box (UZ [mm])
+            - PERC [mm d-1]defines the maximum percolation rate from the upper to the lower groundwater box (LZ [mm])
 
         Parameters
         ----------
@@ -319,7 +315,9 @@ class HBVBergestrom92(BaseConceptualModel):
         return wi
 
     def routing(self, q, maxbas=1):
-        """Routing. This function implements the transfer function using a triangular function.
+        """Routing.
+
+        This function implements the transfer function using a triangular function.
 
         Parameters
         ----------
@@ -347,8 +345,8 @@ class HBVBergestrom92(BaseConceptualModel):
     def simulate(self, prec, temp, et, ll_temp, par, init_st=None, q_init=None, snow=0):
         """Simulate.
 
-            Run the HBV model for the number of steps (n) in precipitation. The
-            resluts are (n+1) simulation of discharge as the model calculates step n+1
+            Run the HBV model for the number of steps (n) in precipitation.
+            The results are (n+1) simulation of discharge as the model calculates the step n+1.
 
 
         Parameters
@@ -364,12 +362,15 @@ class HBVBergestrom92(BaseConceptualModel):
             [ltt, utt, ttm, cfmax, fc, ecorr, etf, lp, k, k1,
             alpha, beta, cwh, cfr, c_flux, perc, rfcf, sfcf]
         init_st : array_like [5], optional
-            Initial model states, [sp, sm, uz, lz, wc]. If unspecified,
+            Initial model states, [sp, sm, uz, lz, wc].
+            If unspecified,
             [0.0, 30.0, 30.0, 30.0, 0.0] mm
         ll_temp : array_like [n], optional
-            Long term average temptearature. If unspecified, calculated from temp.
+            Long term average temptearature.
+            If unspecified, calculated from temp.
         q_init : float, optional
-            Initial discharge value. If unspecified set to 10.0
+            Initial discharge value.
+            If unspecified set to 10.0
         snow: [int]
             1 if snow is considered 0 if not.
 
